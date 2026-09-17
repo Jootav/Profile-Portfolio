ebe1a1fce548a32fecf4102021e46830feaaf9ed
Jootav <jota.victor.barbosa@gmail.com>
2026-09-17 16:40:10 -0300

    Live clock and style changes.

--- teste.js
+++ teste.js
@@ -1,3 +1,10 @@
+function time(){
+    var date=new Date();
+    var current_time=date.toLocaleTimeString();
+    document.getElementById("relogio").innerHTML=current_time;
+}
+setInterval(time,1000);
+
 function changeColorOver(x){
     x.style.backgroundColor = "#000408c1";
 }
@@ -5,3 +12,15 @@ function changeColorOver(x){
 function changeColorOut(x){
     x.style.backgroundColor = "#0004087c";
 }
+
+const ImgHoverScale=document.querySelectorAll('.profile-img, .img-carrossel img');
+
+ImgHoverScale.forEach((img) => {
+  img.addEventListener("mouseover", function() {
+    img.style.scale = "1.05";
+  });
+  
+  img.addEventListener("mouseout", function() {
+    img.style.scale = "1";
+  });
+});
